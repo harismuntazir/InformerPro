2d0a695c0b41c7a22721592f9466e9e383515dfd
hm <haris.0813.m@gmail.com>
2021-03-28 10:55:43 +0530

    Add files via upload

--- app/src/main/java/com/badtools/informer/Downloader.kt
+++ app/src/main/java/com/badtools/informer/Downloader.kt
@@ -12,8 +12,7 @@ import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import kotlinx.coroutines.*
-import java.io.File
-import java.io.FileOutputStream
+import java.io.*
 import java.net.URL
 
 class Downloader : AppCompatActivity() {
@@ -45,7 +44,6 @@ class Downloader : AppCompatActivity() {
         urlEnd = util.getCache(ctx, "urlEnd")
         //paramCtr = util.getCache(ctx, "paramCtr").toInt()
 
-        //findViewById<TextView>(R.id.log).text = "$mainUrl$batch$midUrl$pStart$pEnd$urlEnd"
         val dlBtn = findViewById<Button>(R.id.start_dl)
         dlBtn.setOnClickListener {
             dlBtn.setBackgroundColor(R.color.light_red)
@@ -54,9 +52,102 @@ class Downloader : AppCompatActivity() {
             isDirExists(path)
             path = "${Environment.getStorageDirectory()}/self/primary/Informer/$batch"
             isDirExists(path)
-            startDownloader()
-
+            //the downloader is now in pENd function
+            //so call in the pEnd function
+            pEnd()
         }
+
+    }
+    //finds total number of profiles to be downloaded
+    @RequiresApi(Build.VERSION_CODES.R)
+    private fun pEnd() {
+        var upperLimit = pEnd.toInt()
+        var ctr = 0
+        var isClose = 0
+        var round = 0
+        var lost = true
+        GlobalScope.launch {
+            withContext(Dispatchers.IO) {
+                do {
+                    val data = getData(createUrl(upperLimit.toString()))
+                    ctr++   //keep track for number of turns it took
+                    if (upperLimit < 0) {
+                        break
+                    }
+                    if (data.size > 97510) {
+                        //save this file to phone storage
+                        saveFile(data, makeName(upperLimit.toString()))
+                        when (round) {
+                            0 -> {
+                                upperLimit += 10000
+                            }
+                            1 -> {
+                                isClose = 1
+                                upperLimit += 1000
+                            }
+                            2 -> {
+                                isClose = 2
+                                upperLimit += 100
+                            }
+                            3 -> {
+                                isClose = 3
+                                upperLimit += 10
+                            }
+                            4 -> {
+                                isClose = 4
+                                upperLimit++
+                            }
+                        }
+                    } else {
+                        when (isClose) {
+                            0 -> {
+                                round = 1
+                                upperLimit -= 5000
+                            }
+                            1 -> {
+                                round = 2
+                                upperLimit -= 500
+                            }
+                            2 -> {
+                                round = 3
+                                upperLimit -= 50
+                            }
+                            3 -> {
+                                round = 4
+                                upperLimit -= 5
+                            }
+                            4 -> {
+                                //finally the job is done
+                                upperLimit--
+                                lost = false
+                            }
+                        }
+                    }
+                    runOnUiThread{
+                        findViewById<TextView>(R.id.log).text = upperLimit.toString()
+                    }
+                } while (lost)
+
+                /*
+                runOnUiThread{
+                    if (upperLimit < 0) {
+                        findViewById<TextView>(R.id.log).text = "No Records Found !"
+                    } else {
+                        findViewById<TextView>(R.id.log).text = "$upperLimit Profiles Founds, We Have Started Downloading.."
+                    }
+                }
+                 */
+                if (upperLimit > 0) {
+                    pEnd = upperLimit.toString()
+                    //save upperLimit aka pEnd in cahce
+                    util.setCache(ctx, "pEnd", pEnd)
+                    //start the downloader
+                    startDownloader()
+                }
+            }
+        }
+
+
     }
 
     //the main downloader
